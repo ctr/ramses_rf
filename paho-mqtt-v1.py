686a7e9d5185e5e8f5aaeb3417b9d305319ab0e9
Chris Rodgers <639838+ctr@users.noreply.github.com>
2024-10-18 23:59:22 +0100

    Fix Tx packet JSON format for MQTT. Tx packets are also sent to Rx but with 000 "RSSI", so we ignore them to avoid a flood of packets.

--- paho-mqtt-v1.py
+++ paho-mqtt-v1.py
@@ -26,7 +26,7 @@ def on_connect(client, userdata, flags, rc):
 # Callback function when a message is received
 def on_message(client, userdata, msg):
     message = msg.payload.decode("utf-8")  # Decode the received message
-    print(f"Message received: {message}")
+    print(f"\n\nMessage received: {message}")
 
     msg = json.loads(message)
     print(f"{msg=}")
@@ -38,10 +38,20 @@ def on_message(client, userdata, msg):
 
     print(f"{mm.dst=}")
 
+    # Note. You can manually transmit a temperature packet with
+    # 21.93C
+    # (evohome) ctr28@flint:~/evohome/ramses_rf$ mosquitto_pub -m '{ "msg": " I --- 03:150994 --:------ 03:150994 30C9 003 000891" }' -t "RAMSES/GATEWAY/18:226396/tx"
+    # 18.4C
+    # (evohome) ctr28@flint:~/evohome/ramses_rf$ mosquitto_pub -m '{ "msg": " I --- 03:150994 --:------ 03:150994 30C9 003 000730" }' -t "RAMSES/GATEWAY/18:226396/tx"
+
     # Check if the message contains the DEVICE_ID to forward
-    if DEVICE_ID == str(mm.dst):
+    # Annoyingly transmitted packets are echoed to Rx BUT they have 000 as the RSSI which is impossible I think for normal packets.
+    if packet._rssi == "000":
+        print(f"Ignoring Tx packet echoing back: {mm=}")
+    elif DEVICE_ID == repr(mm.src): # For whatever reason repr doesn't decode the names but str does.
         print(f"Message from DEVICE_ID found: {mm=}. REPEATING!")
-        client.publish(TOPIC_PUBLISH, repr(mm))
+        client.publish(TOPIC_PUBLISH, f'{{ "msg": "{repr(mm)}" }}') # MQTT Tx needs to be in JSON format
+        # Note that in python f-strings {{ gives a literal {, and }} gives a literal }.
     else:
         print(f"Ignoring message: {mm=}")
 
